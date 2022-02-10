f201d5f5691d10c1246ce041792abeb886bd4efa
lcromanini <luiscarloromanini@gmail.com>
2022-02-10 19:35:53 -0300

    activity

--- lesson5/js/task6.js
+++ lesson5/js/task6.js
@@ -87,9 +87,104 @@ button.onclick = function () {
 
 };
 
-function removeAll(){
-    document.getElementsByClassName(".list").innerHTML=" ";
+// function removeAll(){
+//     document.getElementsByClassName(".list").innerHTML=" ";
+// }
+// Dbutton.onclick = removeAll();
+
+/* FETCH */
+
+// Step 1: Declare a global empty array variable to store a list of temples
+let templeList = [];
+// Step 2: Declare a function named output that accepts a list of temples as an array argument and does the following for each temple:
+
+const output = (temples) =>{
+    temples.forEach(temple => {
+        // - Creates an HTML <article> element
+        let article = document.createElement('article');
+        // - Creates an HTML <h3> element and add the temple's templeName property to it
+        let templeName = document.createElement('h3');
+        templeName.textContent = temple.templeName;
+        // - Creates an HTML <h4> element and add the temple's location property to it
+        let location = document.createElement('h4');
+        location.textContent = temple.location;
+        // - Creates an HTML <h4> element and add the temple's dedicated property to it
+        let dedicated = document.createElement('h4');
+        dedicated.textContent = temple.dedicated;
+        // - Creates an HTML <img> element and add the temple's imageUrl property to the src attribute and the temple's templeName property to the alt attribute
+        let image = document.createElement('img');
+        image.setAttribute('src',temple.imageUrl);
+        image.setAttribute('alt',temple.templeName);
+        // - Appends the <h3> element, the two <h4> elements, and the <img> element to the <article> element as children
+        article.appendChild(templeName);
+        article.appendChild(location);
+        article.appendChild(dedicated);
+        article.appendChild(image);
+        // - Appends the <article> element to the HTML element with an ID of temples
+        document.querySelector('#temples').appendChild(article);
+    });
+
+
 }
-Dbutton.onclick = removeAll();
+// Step 3: Using the built-in fetch method, call this absolute URL: 'https://byui-cse.github.io/cse121b-course/week05/temples.json'
+
+// Step 4: Add a .then() method to turn the returned string into a JavaScript object ( hint: .json() )
+
+// Step 5: Add another .then() method with a variable name to hold the temples and an empty arrow function
+
+// Step 6: Inside of second .then() method, assign the list of temples (as a JSON object) to the temples variable
+
+// Step 7: Finally, call the output function and pass it the list of temples
+fetch('https://byui-cse.github.io/cse121b-course/week05/temples.json')
+    .then(response => response.json())
+    .then(temples => {
+        templeList = temples;
+        output(templeList);
+    });
+// Step 8: Declare a function named reset that clears all of the <article> elements from the HTML element with an ID of temples
+const reset = () =>{
+    document.querySelector('#temples').innerHTML = '';
+}  
+// Step 9: Declare a function named sortBy that does the following:
+const sortBy = () => {
+    // - Calls the reset function
+    reset();
+// - Sorts the global temple list by the currently selected value of the HTML element with an ID of sortBy
+// - Calls the output function passing in the sorted list of temples
+    let filter = document.querySelector('#sortBy').value;
+
+    switch (filter) {
+        case 'templeNameAscending':
+            output(templeList.sort(
+                (temple1, temple2) => {
+                    let templeName1 = temple1.templeName.toLowerCase();
+                    let templeName2 = temple2.templeName.toLowerCase();
+                    if (templeName1 < templeName2) return -1;
+                    else if (templeName1 > templeName2) return 1;
+                    else return 0;
+                }));
+            break;
+        case 'templeNameDescending':
+            output(templeList.sort(
+                (temple1, temple2) => {
+                    let templeName1 = temple1.templeName.toLowerCase();
+                    let templeName2 = temple2.templeName.toLowerCase();
+                    if (templeName1 > templeName2) return -1;
+                    else if (templeName1 < templeName2) return 1;
+                    else return 0;
+                }));
+            break;
+        default:
+            // using ternary operators
+            output(templeList.sort(
+                (temple1, temple2) => 
+                temple1.templeName.toLowerCase() > temple2.templeName.toLowerCase() ? 1 : 
+                    temple2.templeName.toLowerCase() > temple1.templeName.toLowerCase() ? -1 : 0));
+            break;
+    }
+}
+
+// Step 10: Add a change event listener to the HTML element with an ID of sortBy that calls the sortBy function
+document.querySelector('#sortBy').addEventListener('change', sortBy);
 
 
